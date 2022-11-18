f2efdffb93f97aece90fa04d61dfc35ef0cc273a
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-11-18 14:04:18 +0530

    rem-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem'
+      }
+    }
   },
   plugins: [require("daisyui")],
 }
